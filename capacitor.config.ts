88f1e33dd220ab8493b158a1379046474dd0d507
Jonathan <j.ramrez12.j@gmail.com>
2026-04-29 20:43:47 -0600

    ZIP con ID y Nombre de App actualizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AhorcadoMv',
+  appId: 'io.ahorcado.steam',
+  appName: 'Ahorcado',
   webDir: 'dist'
 };
 
